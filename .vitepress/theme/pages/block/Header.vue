709edb5efe3ca31187fc44f3db5d4770b5786eca
Eduard Kagan <5306834+kaganem@users.noreply.github.com>
2023-09-21 00:03:12 +0300

    Something starts coming together at last

--- .vitepress/theme/pages/block/Header.vue
+++ .vitepress/theme/pages/block/Header.vue
@@ -1,13 +1,13 @@
 
 <template>
-<table style="width: 84ch;" id="header">
+<!-- <table style="width: 84ch;" id="header">
   <tr>
-    <td class="tl">&nbsp;</td>
+    <td class="tl"></td>
     <td class="tc"></td>
-    <td class="tr">&nbsp;</td>
+    <td class="tr"></td>
   </tr>
   <tr>
-    <td  class="ml">&nbsp;</td>
+    <td  class="ml"></td>
     <td style="text-align: right; padding-right: 1ch;">
 
         <a href="#">Services</a> 
@@ -17,70 +17,127 @@
         <a href="#">Contacts</a>
 
     </td>
-    <td  class="mr">&nbsp;</td>
+    <td  class="mr"></td>
   </tr>
   <tr>
-    <td class="bl">&nbsp;</td>
+    <td class="bl"></td>
     <td class="bc"></td>
-    <td class="br">&nbsp;</td>
+    <td class="br"></td>
   </tr>
-</table>
+</table> -->
+
+ <div class="frame">
+    <div class="line">
+        <div class="wrapper">
+
+
+       
+        <div class="content">
+            <a href="#">Services</a> 
+            <span class="spacer">+</span> 
+            <a href="#">Crew</a> 
+            <span class="spacer">+</span> 
+            <a href="#">Contacts</a>
+        </div>
+    </div>
+    
+    </div>
+ </div>
+
 </template>
 <style>
+
+.frame {
+    padding: calc(0.5em - 1px) calc(0.5ch - 1px) ;
+}
+
+.line {
+    background-color: var(--pgnwr-dark);
+    padding: 2px;
+}
+
+.wrapper {
+    background-color: var(--pgnwr-background);
+    padding: calc(0.5em - 1px) calc(0.5ch - 1px);
+}
+
+.content {
+    padding-right: 1ch;
+    padding-left: 1ch;
+}
+
 table {
     border: 0;
     border-spacing: 0;
 }
-.tl{
-    background-image: url('/assets/img/brdr/corner.png');
-    background-repeat: no-repeat;
-    background-size: contain;
-    width: 1ch;
-}
+
 .tc, .bc {
     background-image: url('/assets/img/brdr/horizontal.png');
     background-repeat: repeat-x;
     background-size: contain;
+    height: 16px;
+    min-width: 8px;
 }
+
+.tc {
+    background-position: left bottom;
+}
+
+.bc {
+    background-position: left top;
+}
+
 
 .ml, .mr {
     background-image: url('/assets/img/brdr/vertical.png');
     background-repeat: repeat-y;
     background-size: contain;
-    width: 1ch;
+    min-height: 16px;
+    width: 8px;
+}
+.mr {
+  background-position: left;
+}
+.ml {
+  background-position: right;
+}
+
+.tl{
+    height: 16px;
+    width: 8px;
+    background-image: url('/assets/img/brdr/corner-tl.png');
+    background-repeat: no-repeat;
+    background-size: contain;
+    
+    background-position: right bottom;
+
 }
 
 .bl{
-    background-image: url('/assets/img/brdr/corner.png');
+    height: 16px;
+    width: 8px;
+    background-image: url('/assets/img/brdr/corner-bl.png');
     background-repeat: no-repeat;
     background-size: contain;
-    -moz-transform: scaleY(-1);
-    -o-transform: scaleY(-1);
-    -webkit-transform: av(-1);
-    transform: scaleY(-1);
-    width: 1ch;
+    background-position: right top;
 } 
 
 .tr{
-    background-image: url('/assets/img/brdr/corner.png');
+    height: 16px;
+    width: 8px;
+    background-image: url('/assets/img/brdr/corner-tr.png');
     background-repeat: no-repeat;
     background-size: contain;
-    -moz-transform: scaleX(-1);
-    -o-transform: scaleX(-1);
-    -webkit-transform: scaleX(-1);
-    transform: scaleX(-1);
-    width: 1ch;
+    background-position: left bottom;
 } 
 
 .br{
-    background-image: url('/assets/img/brdr/corner.png');
+    height: 16px;
+    width: 8px;
+    background-image: url('/assets/img/brdr/corner-br.png');
     background-repeat: no-repeat;
     background-size: contain;
-    -moz-transform: rotate(180deg);
-    -o-transform: rotate(180deg);
-    -webkit-transform: rotate(180deg);
-    transform: rotate(180deg);
-    width: 1ch;
+    background-position: left top;
 } 
 
     .spacer {
